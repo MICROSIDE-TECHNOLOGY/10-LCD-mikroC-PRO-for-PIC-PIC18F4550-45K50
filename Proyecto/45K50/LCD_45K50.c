1b43e5153eaf2b767d4414501b600608b56cc5a3
vhperaltaj <victor.peralta@microside.com>
2023-09-08 10:32:02 -0600

    Fixed indentation

--- Proyecto/45K50/LCD_45K50.c
+++ Proyecto/45K50/LCD_45K50.c
@@ -16,9 +16,9 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
-#pragma orgall 0x1FFF                        //Espacio reservado para bootloader
+#pragma orgall 0x1FFF // Espacio reservado para bootloader
 
-//Referencias externas de conexión del modulo LCD
+// Referencias externas de conexión del modulo LCD
 sbit LCD_RS at LATB0_bit;
 sbit LCD_EN at LATB2_bit;
 sbit LCD_D4 at LATB3_bit;
@@ -37,48 +37,49 @@ sbit LCD_D7_Direction at TRISB6_bit;
 char txt1[] = "MICROSIDE";
 char txt2[] = "TECHNOLOGY";
 
-char i;                         //variable del ciclo for
+char i;                           // variable del ciclo for
 
-void Move_Delay() {             //Función utilizada para el movimiento del texto
-  Delay_ms(500);                //Velocidad de movimiento del texto
+void Move_Delay()
+{                                 // Función utilizada para el movimiento del texto
+   Delay_ms( 500 );               // Velocidad de movimiento del texto
 }
 
 void main() org 0x2000
 {
-  ANSELB  = 0;                  //Configura el puerto B como pines digitales
-  TRISB1_bit = 0;               //Configura el puerto B1 como salida
-  LATB.f1 = 0;                  //Escribimos un 0 para definir el LCD como escritura
-  C1ON_bit = 0;                 //Deshabilita los comparadores
-  C2ON_bit = 0;
-  
-  Lcd_Init();                   //Inicializa el LCD
+   ANSELB = 0;                    // Configura el puerto B como pines digitales
+   TRISB1_bit = 0;                // Configura el puerto B1 como salida
+   LATB.f1 = 0;                   // Escribimos un 0 para definir el LCD como escritura
+   C1ON_bit = 0;                  // Deshabilita los comparadores
+   C2ON_bit = 0;
 
-  Lcd_Cmd(_LCD_CLEAR);          //Limpia el display
-  Lcd_Cmd(_LCD_CURSOR_OFF);     //Posiciona el Cursor
-  Lcd_Out(1,6,txt1);            //Escribe en la primera linea
-  Lcd_Out(2,6,txt2);            //Escribe en la segunda linea
-  Delay_ms(2000);
-  Lcd_Cmd(_LCD_CLEAR);          //limpia el display
+   Lcd_Init();                    // Inicializa el LCD
 
-  Lcd_Out(1,1,txt1);            //Escribe en la primera linea
-  Lcd_Out(2,5,txt2);            //Escribe en la segunda linea
+   Lcd_Cmd( _LCD_CLEAR );         // Limpia el display
+   Lcd_Cmd( _LCD_CURSOR_OFF );    // Posiciona el Cursor
+   Lcd_Out( 1, 6, txt1 );         // Escribe en la primera linea
+   Lcd_Out( 2, 6, txt2 );         // Escribe en la segunda linea
+   Delay_ms( 2000 );
+   Lcd_Cmd( _LCD_CLEAR );         // limpia el display
 
-  Delay_ms(2000);
+   Lcd_Out( 1, 1, txt1 );         // Escribe en la primera linea
+   Lcd_Out( 2, 5, txt2 );         // Escribe en la segunda linea
 
-  for(i=0; i<4; i++) {          //Mueve el texto 4 casillas a la derecha
-    Lcd_Cmd(_LCD_SHIFT_RIGHT);
-    Move_Delay();
-  }
+   Delay_ms( 2000 );
 
-  while(1) {                    //inicia el cliclo infinito
-    for(i=0; i<8; i++) {        //Mueve el texto 7 casillas a la Izquierda
-      Lcd_Cmd(_LCD_SHIFT_LEFT);
+   for ( i = 0; i < 4; i++ ) {    // Mueve el texto 4 casillas a la derecha
+      Lcd_Cmd( _LCD_SHIFT_RIGHT );
       Move_Delay();
-    }
+   }
 
-    for(i=0; i<8; i++) {        //Mueve el texto 7 casillas a la Derecha
-      Lcd_Cmd(_LCD_SHIFT_RIGHT);
-      Move_Delay();
-    }
-  }
+   while ( 1 ) {                  // inicia el cliclo infinito
+      for ( i = 0; i < 8; i++ ) { // Mueve el texto 7 casillas a la Izquierda
+         Lcd_Cmd( _LCD_SHIFT_LEFT );
+         Move_Delay();
+      }
+
+      for ( i = 0; i < 8; i++ ) { // Mueve el texto 7 casillas a la Derecha
+         Lcd_Cmd( _LCD_SHIFT_RIGHT );
+         Move_Delay();
+      }
+   }
 }
